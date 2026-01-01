044fc5265bc88b9d1b55eec193869f6e113686a3
Marcus <owaremarcuskofi@gmail.com>
2026-01-01 03:28:48 +0000

    mvp almost here

--- app/dashboard/notifications/page.tsx
+++ app/dashboard/notifications/page.tsx
@@ -2,27 +2,5 @@
 import NotificationCard from "./notficationlogic";
 
 export default function page() {
-  return (
-    <div className="p-6 space-y-4">
-      <NotificationCard
-        title="New Booking Request"
-        message="A new guest has booked Room 302. Please review."
-        time="2 min ago"
-        unread
-      />
-
-      <NotificationCard
-        title="Payment Received"
-        message="You received ¢150.00 from Jonathan."
-        time="35 min ago"
-      />
-
-      <NotificationCard
-        title="Chat Message"
-        message="Manager: Please update your availability."
-        time="Yesterday"
-        unread={true}
-      />
-    </div>
-  );
+  return <div></div>;
 }
